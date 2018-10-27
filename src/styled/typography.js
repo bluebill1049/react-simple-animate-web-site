3969f5230cb4b3c65080dba8a761f6cd41b9ab5e
bill <bluebill1049@hotmail.com>
2018-10-27 21:49:56 +1100

    include style

---
+++ src/styled/typography.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const H1 = styled.h1`
+  font-size: 34px;
+  font-weight: 300;
+  color: white;
+  text-align: center;
+  margin-bottom: 10px;
+  font-family: 'Fjalla One', sans-serif;
+  letter-spacing: 1px;
+`
+
+export const H2 = styled.h2`
+  font-size: 20px;
+  font-weight: 300;
+  color: white;
+  text-align: center;
+  margin-bottom: 10px;
+  font-family: sans-serif;
+`
